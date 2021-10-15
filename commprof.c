42030f35b333c49f70d6822ff3cf82fc0811fc98
Ioannis <vardas@par.tuwien.ac.at>
2021-10-15 22:00:14 +0200

    I dont know anymore

--- commprof.c
+++ commprof.c
@@ -149,7 +149,6 @@ profile_this(MPI_Comm comm, int count,MPI_Datatype datatype,int prim,
         }
         else{
             PMPI_Comm_rank(comm, &rank);
-            communicator->time_info[prim] += t_elapsed;
             if ( rank == root ){
                 if ( prim != Allgatherv ){
                     communicator->bytes += sum;
