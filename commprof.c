134aa10225b6bdb985044663b0ed483b87920a5f
Ioannis Vardas <johnvardas@hotmail.com>
2021-10-28 14:21:44 +0200

    Barrier at the end of init

--- commprof.c
+++ commprof.c
@@ -211,6 +211,7 @@ _MPI_Init(int *argc, char ***argv){
         mcpt_abort("Comm_set_attr failed at line %s\n",__LINE__);
     }
     communicators[0] = MPI_COMM_WORLD;
+    MPI_Barrier(MPI_COMM_WORLD);
     return ret;
 }
 
