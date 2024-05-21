cdef5e4b55ba2469f76977a70e185dfba9527bd2
Ioannis Vardas <vardas@par.tuwien.ac.at>
2024-05-21 17:02:54 +0200

    Splitting the data for MPI Gather

--- keyval.cpp
+++ keyval.cpp
@@ -1,5 +1,4 @@
 #include <mpi.h>
-#include <functional>
 #include <cstdlib>
 #include <unistd.h>
 #include <cstring>
@@ -40,6 +39,7 @@ typedef struct comm_profiler {
 
 
 typedef struct comm_data{
+    int comm_id;
     int prim;
     int bucketIndex;
     double time;
@@ -50,7 +50,6 @@ typedef struct comm_data{
 typedef struct comm_all{
     char name[NAMELEN];
     int size;
-    std::vector<comm_data> data;
 } comm_all;
 
 // Helper function to create unique key
@@ -62,7 +61,7 @@ typedef struct comm_all{
 
 int getPrimBucketKey(int prim, int bucketIndex)
 {
-    std::cout << "prim = " << prim << ", bucketIndex = " << bucketIndex << " key = " << prim * NUM_BUCKETS + bucketIndex << std::endl;
+    //std::cout << "prim = " << prim << ", bucketIndex = " << bucketIndex << " key = " << prim * NUM_BUCKETS + bucketIndex << std::endl;
     return prim * NUM_BUCKETS + bucketIndex;
 }
 
@@ -105,23 +104,23 @@ void insertOrUpdatePrimBucketInfo(std::unordered_map<int, primBucketInfo>& map,
     }
 }
 
-void printProfilingData(const std::vector<comm_all>& array) {
-    for (const auto& comm : array) {  // Iterate over each comm_all element
-        std::cout << "Communicator Name: " << comm.name << "\n";
-        std::cout << "  Size: " << comm.size << "\n";
+// void printProfilingData(const std::vector<comm_all>& array) {
+//     for (const auto& comm : array) {  // Iterate over each comm_all element
+//         std::cout << "Communicator Name: " << comm.name << "\n";
+//         std::cout << "  Size: " << comm.size << "\n";
 
-        std::cout << "\n  Profiling Data:\n";
-        for (const auto& entry : comm.data) {  // Iterate over each comm_data entry
-            std::cout << "    Primitive: " << prim_names[entry.prim] << std::endl;
-            std::cout << "    Bucket Index: " << entry.bucketIndex << std::endl;
-            std::cout << "    Time: " << entry.time << std::endl;
-            std::cout << "    Num Messages: " << entry.num_messages << std::endl;
-            std::cout << "    Volume: " << entry.volume << std::endl;
-        }
+//         std::cout << "\n  Profiling Data:\n";
+//         for (const auto& entry : comm.data) {  // Iterate over each comm_data entry
+//             std::cout << "    Primitive: " << prim_names[entry.prim] << std::endl;
+//             std::cout << "    Bucket Index: " << entry.bucketIndex << std::endl;
+//             std::cout << "    Time: " << entry.time << std::endl;
+//             std::cout << "    Num Messages: " << entry.num_messages << std::endl;
+//             std::cout << "    Volume: " << entry.volume << std::endl;
+//         }
 
-        std::cout << "--------------------\n";
-    }
-}
+//         std::cout << "--------------------\n";
+//     }
+// }
 
 
 // Define a function to free the prof_metadata object
@@ -192,11 +191,12 @@ int main(int argc, char *argv[])
     MPI_Comm newcomm;
     MPI_Comm_split(MPI_COMM_WORLD, rank, rank, &newcomm);
     init_comm_prof(newcomm, 'S');
-    PMPI_Comm_get_attr(newcomm,keyval[0],&met,&flag);
+    prof_metadata *met2;
+    PMPI_Comm_get_attr(newcomm,keyval[0],&met2,&flag);
     if (flag){
         printf("Rank %d: nsize = %d, comms = %d, id = %c\n",
-               rank,met->size,met->comms,met->id);
-        strcpy(met->name,"SPLIT");
+               rank,met2->size,met2->comms,met2->id);
+        strcpy(met2->name,"SPLIT");
     }
     else {
         printf("Attribute not found\n");
@@ -213,7 +213,9 @@ int main(int argc, char *argv[])
     data = split_comm->map[getPrimBucketKey(1, 2)];
     printf("Rank %d: Updated data in map: time = %f, num_messages = %d, volume = %lu\n", rank, data.time, data.num_messages, data.volume);
 
+    // Place the profiling data into a vector and gather it to rank 0
     std::vector<comm_all> array;
+    std::vector<comm_data> data_array;
     comm_all comm_meta;
     comm_profiler *world_prof;
     PMPI_Comm_get_attr(MPI_COMM_WORLD, keyval[1], &world_prof, &flag);
@@ -229,12 +231,13 @@ int main(int argc, char *argv[])
             if (it != world_prof->map.end()) {
                 // Allocate comm_data struct and copy the data from map
                 comm_data data;
+                data.comm_id = 0;
                 data.prim = i;
                 data.bucketIndex = j;
                 data.num_messages = it->second.num_messages;
                 data.time = it->second.time;
                 data.volume = it->second.volume;
-                comm_meta.data.push_back(data);
+                data_array.push_back(data);
                 printf("Rank %d: Primitive = %d, Bucket = %d, Time = %f, Num Messages = %d, Volume = %lu\n", rank, i, j, it->second.time, it->second.num_messages, it->second.volume);
             }
         }
@@ -251,7 +254,8 @@ int main(int argc, char *argv[])
 
 
     comm_all comm_meta2;
-    PMPI_Comm_get_attr(newcomm, keyval[1], &comm_prof, &flag);
+    comm_profiler *split_prof;
+    PMPI_Comm_get_attr(newcomm, keyval[1], &split_prof, &flag);
     if ( !flag ){
         printf("Map not found\n");
     }
@@ -259,33 +263,165 @@ int main(int argc, char *argv[])
     for (i=0; i<NUM_OF_PRIMS; ++i) {
         for ( j =0; j<NUM_BUCKETS; ++j ){
             // Check if the key exists in the map
-            auto it = comm_prof->map.find(getPrimBucketKey(i, j));
+            auto it = split_prof->map.find(getPrimBucketKey(i, j));
 
-            if (it != comm_prof->map.end()) {
+            if (it != split_prof->map.end()) {
                 // Allocate comm_data struct and copy the data from map
                 comm_data data;
+                data.comm_id = 1;
                 data.prim = i;
                 data.bucketIndex = j;
                 data.num_messages = it->second.num_messages;
                 data.time = it->second.time;
                 data.volume = it->second.volume;
-                comm_meta2.data.push_back(data);
+                data_array.push_back(data);
                 printf("Rank %d: Primitive = %d, Bucket = %d, Time = %f, Num Messages = %d, Volume = %lu\n", rank, i, j, it->second.time, it->second.num_messages, it->second.volume);
             }
         }
     }
-    PMPI_Comm_get_attr(newcomm,keyval[0],&met,&flag);
+    PMPI_Comm_get_attr(newcomm,keyval[0],&met2,&flag);
     if (flag) {
-        strcpy(comm_meta2.name, met->name);
-        comm_meta2.size = met->size;
+        strcpy(comm_meta2.name, met2->name);
+        comm_meta2.size = met2->size;
         array.push_back(comm_meta2);
+        printf("Rank %d: Metadata: %s\n", rank, comm_meta2.name);
     }
     else{
         printf("Rank %d: Metadata not found\n", rank);
     }
 
-    printProfilingData(array);
+    //printProfilingData(array);
 
+    // Gather the profiling metadata from all ranks to rank 0
+    int local_size = array.size();
+    int *c_recvcounts = (int*)malloc(size * sizeof(int));
+    int *c_displs = (int*)malloc(size * sizeof(int));
+    int total_num_of_comms = 0;
+    comm_all dummy;
+    MPI_Gather(&local_size, 1, MPI_INT, c_recvcounts, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    // Root process: Allocate receiving buffer and calculate displacements
+    std::vector<comm_all> recv_buffer;
+    if (rank == 0) {
+        c_displs[0] = 0;
+        total_num_of_comms = c_recvcounts[0];
+        for (i = 1; i < size; ++i) {
+          c_displs[i] = c_displs[i - 1] + c_recvcounts[i - 1];
+          total_num_of_comms += c_recvcounts[i];
+        }
+        std::cout << "mpisee: total number of communicators = " << total_num_of_comms << std::endl;
+        recv_buffer.resize(total_num_of_comms);
+
+    }
+    // Gather the data
+    MPI_Datatype MPI_COMM_ALL;
+    MPI_Datatype types[2] = {MPI_CHAR, MPI_INT};
+    int blocklengths[2] = {NAMELEN, 1};
+    MPI_Aint displacements[2];
+    MPI_Aint base;
+    MPI_Get_address(&dummy, &base);
+    MPI_Get_address(&dummy.name, &displacements[0]);
+    MPI_Get_address(&dummy.size, &displacements[1]);
+   // Convert addresses to displacements
+    for (i = 0; i < 2; i++) {
+        displacements[i] = MPI_Aint_diff(displacements[i], base);
+    }
+
+    MPI_Type_create_struct(2, blocklengths, displacements, types, &MPI_COMM_ALL);
+    MPI_Type_commit(&MPI_COMM_ALL);
+
+    MPI_Gatherv(array.data(), local_size, MPI_COMM_ALL,
+                recv_buffer.data(), c_recvcounts, c_displs, MPI_COMM_ALL, 0, MPI_COMM_WORLD);
+
+    if (rank == 0) {
+        for (i = 0; i < total_num_of_comms; ++i) {
+            std::cout << "Communicator Name: " << recv_buffer[i].name << "\n";
+            std::cout << "  Size: " << recv_buffer[i].size << "\n";
+        }
+    }
+
+    // Free the MPI datatype
+    MPI_Type_free(&MPI_COMM_ALL);
+    // Free the "array" vector
+    array.clear();
+    array.shrink_to_fit(); // recv_buffer has all data now
+
+
+// 1. Create MPI datatype for comm_data
+    comm_data dummy_data;
+    MPI_Datatype MPI_COMM_DATA;
+    MPI_Datatype datatypes[6] = {MPI_INT, MPI_INT, MPI_INT, MPI_DOUBLE, MPI_INT, MPI_UINT64_T};
+    int blocklengths2[6] = {1, 1, 1, 1, 1, 1};
+    MPI_Aint displacements2[6]; // Use more descriptive names for clarity
+    MPI_Aint comm_data_base;    // More descriptive name
+
+    MPI_Get_address(&dummy_data, &comm_data_base);
+
+    MPI_Get_address(&dummy_data.comm_id, &displacements2[0]);
+    MPI_Get_address(&dummy_data.prim, &displacements2[1]);
+    MPI_Get_address(&dummy_data.bucketIndex, &displacements2[2]);
+    MPI_Get_address(&dummy_data.time, &displacements2[3]);
+    MPI_Get_address(&dummy_data.num_messages, &displacements2[4]);
+    MPI_Get_address(&dummy_data.volume, &displacements2[5]);
+
+    for (i = 0; i < 6; ++i) {
+        displacements2[i] = MPI_Aint_diff(displacements2[i], comm_data_base);
+    }
+
+    MPI_Type_create_struct(6, blocklengths2, displacements2, datatypes, &MPI_COMM_DATA);
+
+    MPI_Type_commit(&MPI_COMM_DATA);
+
+    // 2. Gather the profiling data from all ranks to rank 0
+    int local_data_size = data_array.size();
+    int total_num_of_data = 0;
+    int *recvcounts = (int*)malloc(size * sizeof(int));
+    int *displs = (int*)malloc(size * sizeof(int));
+    MPI_Gather(&local_data_size, 1, MPI_INT, recvcounts, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    // Root process: Allocate receiving buffer and calculate displacements
+    std::vector<comm_data> recv_data_buffer;
+    if (rank == 0) {
+        displs[0] = 0;
+        total_num_of_data = recvcounts[0];
+        for (i = 1; i < size; ++i) {
+          displs[i] = displs[i - 1] + recvcounts[i - 1];
+          total_num_of_data += recvcounts[i];
+        }
+        std::cout << "mpisee: total number of data = " << total_num_of_data << std::endl;
+        recv_data_buffer.resize(total_num_of_data);
+    }
+    // Gather the data
+    MPI_Gatherv(data_array.data(), local_data_size, MPI_COMM_DATA,
+                recv_data_buffer.data(), recvcounts, displs, MPI_COMM_DATA, 0, MPI_COMM_WORLD);
+    int procs;
+    int comms_per_proc;
+    int data_per_procs;
+    if (rank == 0) {
+        for ( procs = 0; procs < size; ++procs ){
+            comms_per_proc = c_recvcounts[procs];
+            data_per_procs = recvcounts[procs];
+            for (i = 0; i < comms_per_proc; ++i) {
+                std::cout << "Communicator Name: " << recv_buffer[c_displs[procs] + i].name << "\n";
+                std::cout << "  Size: " << recv_buffer[c_displs[procs] + i].size << "\n";
+                for (j = 0; j < data_per_procs; ++j) {
+                    if ( recv_data_buffer[displs[procs] + j].comm_id != i ) {
+                        continue;
+                    }
+                    std::cout << "    Primitive: " << prim_names[recv_data_buffer[displs[procs] + j].prim] << "\n";
+                    std::cout << "    Bucket Index: " << recv_data_buffer[displs[procs] + j].bucketIndex << "\n";
+                    std::cout << "    Time: " << recv_data_buffer[displs[procs] + j].time << "\n";
+                    std::cout << "    Num Messages: " << recv_data_buffer[displs[procs] + j].num_messages << "\n";
+                    std::cout << "    Volume: " << recv_data_buffer[displs[procs] + j].volume << "\n";
+                }
+            }
+        }
+    }
+
+    MPI_Type_free(&MPI_COMM_DATA);
+    // Free the arrays you allocated
+    free(recvcounts);
+    free(displs);
+    free(c_recvcounts);
+    free(c_displs);
     MPI_Finalize();
     return 0;
 }
