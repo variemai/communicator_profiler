ed6d3fed39ec7581c287d1af94f29e88438c64fb
Ioannis Vardas <vardas@par.tuwien.ac.at>
2024-05-21 13:53:04 +0200

    Cartesian key

--- keyval.cpp
+++ keyval.cpp
@@ -6,16 +6,19 @@
 #include <unordered_map>
 #include <cstdint>
 #include <cstdio>
+#include <vector>
+#include <iostream>
 
 #define NAMELEN 32
 #define NUM_OF_PRIMS 6 // Simulate 6 primitives
-#define NUM_BUCKETS 8
+#define NUM_BUCKETS 4
 #define KEY_START_VALUE 100
 
 int local_cid = 0;
 int keyval[2]; // keyval[0]  contains metadata
-                   // keyval[1]  contains profiling data
-const int64_t buckets[NUM_BUCKETS-1] = {128,1024,8192,65536,262144,1048576,33554432};
+               // keyval[1]  contains profiling data
+int64_t buckets[NUM_BUCKETS-1] = {128,1024,8192};
+const char prim_names[][NAMELEN] = {"Send", "Recv", "Allreduce", "Bcast", "Alltoall", "Reduce"};
 // Metadata associated with an MPI communicator
 typedef struct profiler_metadata {
     char name[NAMELEN];
@@ -35,12 +38,32 @@ typedef struct comm_profiler {
     std::unordered_map<int, primBucketInfo> map;
 } comm_profiler;
 
+
+typedef struct comm_data{
+    int prim;
+    int bucketIndex;
+    double time;
+    int num_messages;
+    uint64_t volume;
+} comm_data;
+
+typedef struct comm_all{
+    char name[NAMELEN];
+    int size;
+    std::vector<comm_data> data;
+} comm_all;
+
 // Helper function to create unique key
-int getPrimBucketKey(int prim, int bucketIndex) {
-    std::hash<int> hash_int;
-    size_t combinedHash = hash_int(prim) ^ (hash_int(bucketIndex) << 1);
-    int keyRangeSize = NUM_OF_PRIMS * NUM_BUCKETS;
-    return combinedHash % keyRangeSize;
+// int getPrimBucketKey(int prim, int bucketIndex) {
+//     std::hash<int> hash_int;
+//     size_t combinedHash = hash_int(prim) ^ (hash_int(bucketIndex) << 1);
+//     return combinedHash;
+// }
+
+int getPrimBucketKey(int prim, int bucketIndex)
+{
+    std::cout << "prim = " << prim << ", bucketIndex = " << bucketIndex << " key = " << prim * NUM_BUCKETS + bucketIndex << std::endl;
+    return prim * NUM_BUCKETS + bucketIndex;
 }
 
 void init_comm_prof(MPI_Comm comm, char c) {
@@ -67,6 +90,10 @@ void insertOrUpdatePrimBucketInfo(std::unordered_map<int, primBucketInfo>& map,
     // Check if the key exists in the map
     auto it = map.find(key);
 
+    if (map.count(key) > 1) {
+        std::cerr << "Hash collision detected for key " << key << std::endl;
+    }
+
     if (it == map.end()) {
         // Key not found, insert the new pair
         map[key] = newInfo;
@@ -75,6 +102,24 @@ void insertOrUpdatePrimBucketInfo(std::unordered_map<int, primBucketInfo>& map,
         it->second.time += newInfo.time;
         it->second.num_messages += newInfo.num_messages;
         it->second.volume += newInfo.volume;
+    }
+}
+
+void printProfilingData(const std::vector<comm_all>& array) {
+    for (const auto& comm : array) {  // Iterate over each comm_all element
+        std::cout << "Communicator Name: " << comm.name << "\n";
+        std::cout << "  Size: " << comm.size << "\n";
+
+        std::cout << "\n  Profiling Data:\n";
+        for (const auto& entry : comm.data) {  // Iterate over each comm_data entry
+            std::cout << "    Primitive: " << prim_names[entry.prim] << std::endl;
+            std::cout << "    Bucket Index: " << entry.bucketIndex << std::endl;
+            std::cout << "    Time: " << entry.time << std::endl;
+            std::cout << "    Num Messages: " << entry.num_messages << std::endl;
+            std::cout << "    Volume: " << entry.volume << std::endl;
+        }
+
+        std::cout << "--------------------\n";
     }
 }
 
@@ -96,7 +141,7 @@ void delete_comm_profiler(MPI_Comm comm, int keyval, void* attr_val, void* extra
 int main(int argc, char *argv[])
 {
     int rank, size;
-    int i;
+    int i,j;
     MPI_Init(&argc,&argv);
 
     MPI_Comm_rank(MPI_COMM_WORLD,&rank);
@@ -119,6 +164,8 @@ int main(int argc, char *argv[])
     if (flag){
         printf("Rank %d: nsize = %d, comms = %d, id = %c\n",
                rank,met->size,met->comms,met->id);
+
+        strcpy(met->name,"WORLD");
     }
     else {
         printf("Attribute not found\n");
@@ -132,24 +179,15 @@ int main(int argc, char *argv[])
     }
     printf("Rank %d: Inserting data into map\n", rank);
     // insert data into map
-    insertOrUpdatePrimBucketInfo(comm_prof->map, getPrimBucketKey(1, 2), 1.0,  1000);
-    //comm_prof->map[getPrimBucketKey(1, 2)] = profData;
-    // printf("Rank %d: Inserted data into map\n", rank);
-    // // get inserted data
+    insertOrUpdatePrimBucketInfo(comm_prof->map, getPrimBucketKey(1, 2), 1.0,  500);
     insertOrUpdatePrimBucketInfo(comm_prof->map, getPrimBucketKey(1, 2), 2.0,  1000);
     insertOrUpdatePrimBucketInfo(comm_prof->map, getPrimBucketKey(0, 0), 5.0, 100);
 
-    // primBucketInfo profData2;
-    // profData2.time = 2.0;
-    // profData2.num_messages = 20;
-    // profData2.volume = 2000;
-
-    // insertOrUpdatePrimBucketInfo(comm_prof->map, getPrimBucketKey(1, 2), profData2);
     primBucketInfo data;
     data = comm_prof->map[getPrimBucketKey(1, 2)];
-    printf("Rank %d: Updated data in map: time = %f, num_messages = %d, volume = %lu\n", rank, data.time, data.num_messages, data.volume);
+    printf("Rank %d: Updated data in map: time = %f, num_messages = %d, volume = %llu\n", rank, data.time, data.num_messages, data.volume);
     data = comm_prof->map[getPrimBucketKey(0, 0)];
-    printf("Rank %d: Updated data in map: time = %f, num_messages = %d, volume = %lu\n", rank, data.time, data.num_messages, data.volume);
+    printf("Rank %d: Updated data in map: time = %f, num_messages = %d, volume = %llu\n", rank, data.time, data.num_messages, data.volume);
 
     MPI_Comm newcomm;
     MPI_Comm_split(MPI_COMM_WORLD, rank, rank, &newcomm);
@@ -158,45 +196,95 @@ int main(int argc, char *argv[])
     if (flag){
         printf("Rank %d: nsize = %d, comms = %d, id = %c\n",
                rank,met->size,met->comms,met->id);
+        strcpy(met->name,"SPLIT");
     }
     else {
         printf("Attribute not found\n");
     }
-    PMPI_Comm_get_attr(newcomm, keyval[1], &comm_prof, &flag);
+    comm_profiler *split_comm;
+    PMPI_Comm_get_attr(newcomm, keyval[1], &split_comm, &flag);
     if (flag) {
         printf("Rank %d: Found the map\n", rank);
     } else {
         printf("Rank %d: Map not found\n", rank);
     }
-    insertOrUpdatePrimBucketInfo(comm_prof->map, getPrimBucketKey(1, 2), 0.5,  10);
-    insertOrUpdatePrimBucketInfo(comm_prof->map, getPrimBucketKey(1, 2), 5.0, 100);
-    data = comm_prof->map[getPrimBucketKey(1, 2)];
+    insertOrUpdatePrimBucketInfo(split_comm->map, getPrimBucketKey(1, 2), 0.5,  10);
+    insertOrUpdatePrimBucketInfo(split_comm->map, getPrimBucketKey(1, 2), 5.0, 100);
+    data = split_comm->map[getPrimBucketKey(1, 2)];
     printf("Rank %d: Updated data in map: time = %f, num_messages = %d, volume = %lu\n", rank, data.time, data.num_messages, data.volume);
 
-    // prof_metadata metadata2;
-    // strncpy(metadata2.name, "SPLIT", NAMELEN);
-    // metadata2.size = size;
-    // metadata2.comms = 1;
-    // metadata2.id = 'T';
+    std::vector<comm_all> array;
+    comm_all comm_meta;
+    comm_profiler *world_prof;
+    PMPI_Comm_get_attr(MPI_COMM_WORLD, keyval[1], &world_prof, &flag);
+    if ( !flag ){
+        printf("Map not found\n");
+    }
+    printf("WORLD\n");
+    for (i=0; i<NUM_OF_PRIMS; ++i) {
+        for ( j =0; j<NUM_BUCKETS; ++j ){
+            // Check if the key exists in the map
+            auto it = world_prof->map.find(getPrimBucketKey(i, j));
 
-    // prof_metadata *met2;
-    // PMPI_Comm_set_attr(newcomm,keyval[0],&metadata2);
-    // PMPI_Comm_get_attr(newcomm,keyval[0],&met2,&flag);
-    // if (flag){
-    //     printf("Rank %d: name = %s, size = %d, comms = %d, id = %c\n",
-    //            rank,met2->name,met2->size,met2->comms,met2->id);
-    // }
-    // else {
-    //     printf("Attribute not found\n");
-    // }
-    // PMPI_Comm_set_attr(newcomm, keyval[1], &prof);
+            if (it != world_prof->map.end()) {
+                // Allocate comm_data struct and copy the data from map
+                comm_data data;
+                data.prim = i;
+                data.bucketIndex = j;
+                data.num_messages = it->second.num_messages;
+                data.time = it->second.time;
+                data.volume = it->second.volume;
+                comm_meta.data.push_back(data);
+                printf("Rank %d: Primitive = %d, Bucket = %d, Time = %f, Num Messages = %d, Volume = %lu\n", rank, i, j, it->second.time, it->second.num_messages, it->second.volume);
+            }
+        }
+    }
+    PMPI_Comm_get_attr(MPI_COMM_WORLD,keyval[0],&met,&flag);
+    if (flag) {
+        strcpy(comm_meta.name, met->name);
+        comm_meta.size = met->size;
+        array.push_back(comm_meta);
+    }
+    else{
+        printf("Rank %d: Metadata not found\n", rank);
+    }
 
-    // PMPI_Comm_get_attr(newcomm, keyval[1], &comm_prof, &flag);
-    // comm_prof->map[getPrimBucketKey(2, 3)] = profData;
-    // data = comm_prof->map[getPrimBucketKey(2, 3)];
-    // printf("Rank %d: Retrieved data from map: time = %f, num_messages = %d, volume = %lu\n", rank, data.time, data.num_messages, data.volume);
 
+    comm_all comm_meta2;
+    PMPI_Comm_get_attr(newcomm, keyval[1], &comm_prof, &flag);
+    if ( !flag ){
+        printf("Map not found\n");
+    }
+    printf("SPLIT\n");
+    for (i=0; i<NUM_OF_PRIMS; ++i) {
+        for ( j =0; j<NUM_BUCKETS; ++j ){
+            // Check if the key exists in the map
+            auto it = comm_prof->map.find(getPrimBucketKey(i, j));
 
+            if (it != comm_prof->map.end()) {
+                // Allocate comm_data struct and copy the data from map
+                comm_data data;
+                data.prim = i;
+                data.bucketIndex = j;
+                data.num_messages = it->second.num_messages;
+                data.time = it->second.time;
+                data.volume = it->second.volume;
+                comm_meta2.data.push_back(data);
+                printf("Rank %d: Primitive = %d, Bucket = %d, Time = %f, Num Messages = %d, Volume = %lu\n", rank, i, j, it->second.time, it->second.num_messages, it->second.volume);
+            }
+        }
+    }
+    PMPI_Comm_get_attr(newcomm,keyval[0],&met,&flag);
+    if (flag) {
+        strcpy(comm_meta2.name, met->name);
+        comm_meta2.size = met->size;
+        array.push_back(comm_meta2);
+    }
+    else{
+        printf("Rank %d: Metadata not found\n", rank);
+    }
+
+    printProfilingData(array);
 
     MPI_Finalize();
     return 0;
