2492dc1cd9d410e88163f0be14e3193ab5598156
Ioannis <vardas@par.tuwien.ac.at>
2024-05-24 07:07:19 +0200

    this can be the solution

--- commprof.cpp
+++ commprof.cpp
@@ -32,8 +32,9 @@ std::unordered_map<MPI_Request, MPI_Comm> requests_map;
 std::vector<prof_attrs*> local_communicators;
 std::unordered_map<MPI_Win, MPI_Comm> comm_map;
 std::vector<MPI_Comm> comms_table;
-MPI_Request free_requests[1000];
-int request_index = 0;
+// MPI_Request free_requests[1000];
+// int request_index = 0;
+std::vector<std::pair<MPI_Group, prof_attrs*>> group_table;
 
 // int global_rank; // For debugging purposes
 
@@ -1141,22 +1142,28 @@ MPI_Comm_free(MPI_Comm *comm)
 {
     int ret,flag;
     prof_attrs *com_info, *prev_info;
-    MPI_Comm newcomm;
-
+    // MPI_Comm newcomm;
+    MPI_Group group;
     // Duplicate the communicator before free
-    PMPI_Comm_idup(*comm, &newcomm, &free_requests[request_index]);
-    request_index++;
+    // PMPI_Comm_idup(*comm, &newcomm, &free_requests[request_index]);
+    // request_index++;
     // MPI_Wait(&request, MPI_STATUS_IGNORE);
-    if (newcomm == MPI_COMM_NULL) {
-        mcpt_abort("Comm_free: Comm_idup failed\n");
-    }
+    // if (newcomm == MPI_COMM_NULL) {
+    //     mcpt_abort("Comm_free: Comm_idup failed\n");
+    // }
+
+
+    // Get the group of the communicator before freeing it
+    PMPI_Comm_group(*comm, &group);
+
 
     // Attach the attributes of the old communicator to the new communicator
     PMPI_Comm_get_attr(*comm, namekey(), &prev_info, &flag);
     com_info = (prof_attrs *)malloc(sizeof(prof_attrs));
     memcpy(com_info, prev_info, sizeof(prof_attrs));
-    PMPI_Comm_set_attr(newcomm, namekey(), com_info);
-    comms_table.push_back(newcomm);
+    group_table.push_back({group, com_info});
+    // PMPI_Comm_set_attr(newcomm, namekey(), com_info);
+    // comms_table.push_back(newcomm);
 
     if (flag) {
         auto it = std::find(local_communicators.begin(), local_communicators.end(), prev_info);
@@ -1222,29 +1229,40 @@ _Finalize(void) {
     if (rc != MPI_SUCCESS) {
         mcpt_abort("MPI_Barrier failed\n");
     }
-
+    MPI_Comm newcomm;
     PMPI_Comm_rank(MPI_COMM_WORLD, &rank);
     PMPI_Comm_size(MPI_COMM_WORLD, &size);
     num_of_comms = local_communicators.size();
 
-    int num_requests = request_index;
+    // int num_requests = request_index;
     // Wait for all duplications in MPI_Comm_free to complete
-    std::cout << "mpisee: Waiting for all MPI_Comm_free requests to complete" << std::endl;
-    std::cout << "mpisee: num_requests = " << num_requests << std::endl;
-    for ( i =0; i<num_requests; i++ ){
-        if ( free_requests[i] == MPI_REQUEST_NULL )
-            continue;
+    // std::cout << "mpisee: Waiting for all MPI_Comm_free requests to complete" << std::endl;
+    // std::cout << "mpisee: num_requests = " << num_requests << std::endl;
+    // for ( i =0; i<num_requests; i++ ){
+    //     if ( free_requests[i] == MPI_REQUEST_NULL )
+    //         continue;
         // if ( free_requests[i] != MPI_REQUEST_NULL ) {
-        PMPI_Wait(&free_requests[i], MPI_STATUS_IGNORE);
+        // PMPI_Wait(&free_requests[i], MPI_STATUS_IGNORE);
         // }
         // else {
         //     std::cout << "mpisee: request_array[" << i << "] is MPI_REQUEST_NULL" << std::endl;
         // }
-    }
+    // }
     // int ret = PMPI_Waitall(num_requests, request_array, MPI_STATUSES_IGNORE);
     // if (ret != MPI_SUCCESS) {
     //     mcpt_abort("MPI_Waitall failed\n");
     // }
+    //
+    // Re-create the communicators from the group_table
+    std::cout << "mpisee: group_table size = " << group_table.size() << std::endl;
+    for (i = 0; i < group_table.size(); ++i) {
+        PMPI_Comm_create_group(MPI_COMM_WORLD, group_table[i].first, 0, &newcomm);
+        if (newcomm == MPI_COMM_NULL) {
+            mcpt_abort("Comm_create_group failed\n");
+        }
+        PMPI_Comm_set_attr(newcomm, namekey(), group_table[i].second);
+        comms_table.push_back(newcomm);
+    }
 
     // std::cout << "mpisee: comms_table size = " << comms_table.size() << std::endl;
     // Iterate over the communicator and call an MPI_Allreduce
